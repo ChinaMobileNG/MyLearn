75d52af3c8c81b03b20f3477be457b66f79ae61a
ShiLei <rock.shi@sap.com>
2016-08-30 10:03:37 +0800

    change the data range to 100 - 2100

--- src/cn/algorithms/chapter1/HashTable.java
+++ src/cn/algorithms/chapter1/HashTable.java
@@ -12,7 +12,7 @@ public class HashTable {
 	public static void  main(String[] aa){
 		Random intRandom = new Random();
 		for(int i=0;i<=100;i++){
-			int num = intRandom.nextInt(200)+100;
+			int num = intRandom.nextInt(2000)+100;
 			double pow = Math.pow(num, 2);
 			System.out.println(num+" "+pow+" "+Double.valueOf(pow).toString().substring(1, 4));
 		}

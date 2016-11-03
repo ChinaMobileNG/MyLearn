1f7abf225d6fd7b39b2d430754e41bf0356b9b4f
Rock <rock.shi@sap.com>
2016-11-03 19:57:15 +0800

    16.19:57
    queue test

--- src/cn/algorithms/chapter2/CircularArrayQueueTest.java
+++ src/cn/algorithms/chapter2/CircularArrayQueueTest.java
@@ -16,6 +16,16 @@ public class CircularArrayQueueTest {
 			System.out.print(" 出队："+myQueue.peek());
 			System.out.println(" 出队后长度："+myQueue.size());
 		}
+//		System.out.println(myQueue.size());
+//		myQueue.put("ONE");
+//		myQueue.put("TWO");
+//		System.out.println(myQueue.size());
+//		System.out.println(myQueue.peek());
+//		System.out.println(myQueue.size());
+//		myQueue.put("THREE");
+//		System.out.println(myQueue.size());
+//		System.out.println(myQueue.peek());
+//		System.out.println(myQueue.size());
 	}
 
 }
---
+++ src/cn/easytest/EasiestTest.java
@@ -0,0 +1,17 @@
+package cn.easytest;
+
+public class EasiestTest {
+
+	private static int a;
+	
+	private static int temp;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		temp = 10;
+		a=temp;
+		temp=20;
+		System.out.println(a);
+	}
+
+}
